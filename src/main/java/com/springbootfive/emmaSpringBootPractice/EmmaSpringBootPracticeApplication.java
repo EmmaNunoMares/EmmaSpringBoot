7f22713c827009c2c4cd0965b6d672fa8825b47c
EmmaNunoMares <emmanuelnunomares@gmail.com>
2020-03-21 18:14:15 -0600

    spring initialazer java 11, spring web, h2

---
+++ src/main/java/com/springbootfive/emmaSpringBootPractice/EmmaSpringBootPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.springbootfive.emmaSpringBootPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmmaSpringBootPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmmaSpringBootPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootfive/emmaSpringBootPractice/EmmaSpringBootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootfive.emmaSpringBootPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmmaSpringBootPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
